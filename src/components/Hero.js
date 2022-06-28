755012260e01c2fcdea8ec519c69135f794223f4
Akindele Ayorinde Emmanuel <73663718+Hayor4real@users.noreply.github.com>
2022-06-28 09:10:10 +0200

    added styling to the hero component

---
+++ src/components/Hero.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Hero.css';
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+      <div className='content'>
+        <p>Call us</p>
+        <p>+491705938069</p>
+        <p>What is finance and accounting</p>
+        <p>
+          In finance and accounting, cash refers to money (currency) that is
+          readily available for use. It may be kept in physical form, digital
+          form, or invested in a short-term money market product. In economics,
+          cash refers only to money that is in the physical form. Cash in
+          Business Operations Cash is the lifeblood of a business.
+        </p>
+        <button href='/' className='button'>
+          Consultation
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
